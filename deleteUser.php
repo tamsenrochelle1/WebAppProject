ced3f9f361c6bbe2611cbb7eb1a57fc15ccdd489
ethanlarsen <30578616+ethanlarsen@users.noreply.github.com>
2020-04-18 16:04:21 -0600

    Update deleteUser.php

--- deleteUser.php
+++ deleteUser.php
@@ -9,9 +9,9 @@ require_once 'inc/menu.php';
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die($conn->connect_error);
 
-if(isset($_POST['member_Id'])) {
-	$memberId=get_post($conn, 'member_Id');
-	$query="DELETE FROM member WHERE member_ID='$member_Id'";
+if(isset($_POST['member_id'])) {
+	$member_id=get_post($conn, 'member_id');
+	$query="DELETE FROM member WHERE member_id='$member_id'";
 	$result=$conn->query($query);
 	if(!$result) echo "DELETE failed: $query <br>" .
 	$conn->error . "<br><br>";
