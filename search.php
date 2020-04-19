0a841191dcdd3e127de5eeed3f731c2def4de99c
Tamsen Reid <46758925+tamsenrochelle1@users.noreply.github.com>
2020-04-19 10:18:30 -0600

    Update search.php
    still not working..

--- search.php
+++ search.php
@@ -5,11 +5,15 @@
 	require_once 'inc/checksession.php';
 	require_once 'inc/menu.php';
 
+	$conn = new mysqli($hn, $un, $pw, $db);
+	if($conn->connect_error) {die($conn->connect_error);}
+
+
 if (!isset($_GET['search'])){ //could also use POST
   header("Location:inventory.php");
 }
 $search_sql="SELECT item_description FROM book WHERE item_description LIKE '%".$_POST['search']."%'";
-$search_query=mysql_query($search_sql);
+$search_query=mysqli_query($search_sql);
     if(mysql_num_rows($search_query) !=0) {
 $search_rs=mysql_fetch_assoc($search_query);
 }
