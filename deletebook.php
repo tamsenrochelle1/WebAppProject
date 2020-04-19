f335062d74e6f98c655f46430f73d18a6bf289f1
CBaddley <62369843+CBaddley@users.noreply.github.com>
2020-04-19 14:24:39 -0600

    Add files via upload

---
+++ deletebook.php
@@ -0,0 +1,34 @@
+<?php
+
+$page_roles = array('admin');
+
+require_once 'inc/menu.php';
+require_once 'dblogin.php';
+require_once 'inc/checksession.php';
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if($conn->connect_error) die($conn->connect_error);
+
+if(isset($_GET['item_id'])) {
+	$item_id = $_GET['item_id'];
+	
+	$query = "delete from book where item_id=$item_id";
+	
+	$result = $conn->query($query);
+	if(!$result) die($conn->error);	
+}
+	
+$result = $conn->query($query);
+if(!$result) die($conn->error);	
+	
+$conn->close();	
+
+header("Location: categories.php");	
+	
+
+function get_post($conn, $var) {
+return $conn->real_escape_string($_POST[$var]);
+}
+
+
+?>
