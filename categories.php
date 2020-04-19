5306bec4a767dd13fc898c6e12f0cfafa7e1c02a
CBaddley <62369843+CBaddley@users.noreply.github.com>
2020-04-18 19:26:14 -0600

    Update categories.php

--- categories.php
+++ categories.php
@@ -4,12 +4,7 @@
 </head>
 
 <body >
-<?php 
-	$page_roles = array('member','admin');
-	require_once 'dblogin.php'; 
-	require_once 'inc/checksession.php';
-	require_once 'inc/menu.php'; 
-	?>
+<?php require_once 'inc/menu.php'; ?>
 
 
 <div class="row">
@@ -30,7 +25,7 @@
   <div class="row">
       <div class="col-md-3">
           <div class="card mb-3">
-              <a href="inventory.php"><img src="images/book.jpg" class="card-img-top" alt="Books"></a>
+              <a href="bookinventory.php"><img src="images/book.jpg" class="card-img-top" alt="Books"></a>
               <div class="card-body">
                   <h5 class="card-title">Books</h5>
               </div>
@@ -38,7 +33,7 @@
       </div>
       <div class="col-md-3">
           <div class="card mb-3">
-              <a href="inventory.php"><img src="images/movie.jpg" class="card-img-top" alt="Movie"></a>
+              <a href="filminventory.php"><img src="images/movie.jpg" class="card-img-top" alt="Movie"></a>
               <div class="card-body">
                   <h5 class="card-title">Movies</h5>
               </div>
@@ -46,7 +41,7 @@
       </div>
       <div class="col-md-3">
           <div class="card mb-3">
-              <a href="inventory.php"><img src="images/magazine.jpg" class="card-img-top" alt="Magazine"></a>
+              <a href="magazineinventory.php"><img src="images/magazine.jpg" class="card-img-top" alt="Magazine"></a>
               <div class="card-body">
                   <h5 class="card-title">Magazines</h5>
               </div>
@@ -57,7 +52,7 @@
     <div class="row">
   <div class="col-md-3">
           <div class="card mb-3">
-              <a href="inventory.php"><img src="images/audiobook.jpg" class="card-img-top" alt="AudioBooks"></a>
+              <a href="audiobookinventory.php"><img src="images/audiobook.jpg" class="card-img-top" alt="AudioBooks"></a>
               <div class="card-body">
                   <h5 class="card-title">Audiobooks</h5>
               </div>
@@ -65,7 +60,7 @@
       </div>
       <div class="col-md-3">
           <div class="card mb-3">
-              <a href="inventory.php"><img src="images/equipment.jpg" class="card-img-top" alt="Equipment"></a>
+              <a href="equipmentinventory.php"><img src="images/equipment.jpg" class="card-img-top" alt="Equipment"></a>
               <div class="card-body">
                   <h5 class="card-title">Equipment</h5>
               </div>
@@ -73,7 +68,7 @@
       </div>
       <div class="col-md-3">
           <div class="card mb-3">
-              <a href="inventory.php"><img src="images/music.jpg" class="card-img-top" alt="Music"></a>
+              <a href="musicinventory.php"><img src="images/music.jpg" class="card-img-top" alt="Music"></a>
               <div class="card-body">
                   <h5 class="card-title">Music</h5>
               </div>
@@ -90,6 +85,7 @@
 
 <?php
    require_once 'inc/footer.php';
-
+	require_once 'inc/checksession.php';
+	$page_roles = array('member', 'admin');
 
   ?>
